15779a59e0cef46c541aed8989d1a69572e23c94
Ben Lewis <bennlewis@gmail.com>
2014-10-18 16:08:45 -0600

    update minesweeper

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,5 +1,6 @@
 def board(raw_board):
-    return Board(raw_board).printout()
+    return Board(raw_board).hint()
+
 
 class Board:
     VALID_BORDERS = ["+", "-", "|"]
@@ -7,38 +8,56 @@ class Board:
 
     def __init__(self, raw_board):
         self.rows = raw_board
-        self._validate()
+        self.__validate()
 
-    def printout(self):
+    def hint(self):
         printout = []
         for i, row in enumerate(self.rows):
-            inner = []
-            for j, space in enumerate(row):
-                if space is not " ":
-                    inner.append(space)
-                else:
-                    counter = 0
-                    for datum in ([row[j -1], row[j + 1], self.rows[i - 1][j - 1], self.rows[i - 1][j], self.rows[i - 1][j + 1], self.rows[i + 1][j - 1], self.rows[i + 1][j], self.rows[i + 1][j + 1]]):
-                        if datum is "*":
-                            counter += 1
-                    if counter > 0:
-                        inner.append(str(counter))
-                    else:
-                        inner.append(space)
-            printout.append("".join(inner))
+            printout.append(self.__hint_row(i, row))
         return printout
 
-    def _validate(self):
+    def __hint_row(self, i, row):
+        inner = []
+        for j, tile in enumerate(row):
+            inner.append(self.__hint_tile(tile, i, j, row))
+        return "".join(inner)
+
+    def __hint_tile(self, tile, i, j, row):
+        if tile is not " ":
+            return tile
+        count = self.__get_bomb_count(row, i, j)
+        if count > 0:
+            return str(count)
+        return tile
+
+    def __get_bomb_count(self, row, i, j):
+        return sum(datum is "*" for datum in
+                   self.__surrounding_squares(row, i, j))
+
+    def __surrounding_squares(self, row, i, j):
+        return [row[j - 1], row[j + 1],
+                self.rows[i - 1][j - 1], self.rows[i - 1][j],
+                self.rows[i - 1][j + 1], self.rows[i + 1][j - 1],
+                self.rows[i + 1][j], self.rows[i + 1][j + 1]]
+
+    def __validate(self):
+        self.__validate_row_lengths()
+        self.__validate_borders()
+        self.__validate_tiles()
+
+    def __validate_row_lengths(self):
         length = len(self.rows[0])
-        if not all(len(row) is length for row in self.rows):
-            raise ValueError("Invalid board")
+        [self.__raise_invalid() for row in self.rows if len(row) is not length]
 
+    def __validate_borders(self):
         for row in ([self.rows[0], self.rows[-1]]):
-            for char in row:
-                if char not in self.VALID_BORDERS:
-                    raise ValueError("Invalid board")
+            [self.__raise_invalid() for char in row
+             if char not in self.VALID_BORDERS]
 
+    def __validate_tiles(self):
         for row in self.rows:
-            for char in row:
-                if char not in self.VALID_DATA:
-                    raise ValueError("Invalid board")
+            [self.__raise_invalid() for tile in row
+             if tile not in self.VALID_DATA]
+
+    def __raise_invalid(self):
+        raise ValueError("Invalid board")
